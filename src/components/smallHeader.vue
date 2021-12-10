27d42e285671c98c705ccc70f52a0f74f69ac0ff
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-10 11:45:14 +0300

    Added small header and main text block for .md templates

---
+++ src/components/smallHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="tags">
+    <span class="tag">
+      <slot/>
+    </span>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    margin-bottom: 0;
+  }
+  .tag {
+    font-size: 1rem;
+    font-weight: bold;
+  }
+</style>
