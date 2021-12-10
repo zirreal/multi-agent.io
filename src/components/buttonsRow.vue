2c6d9d6c821ec71bac55930d79deec6815ba7d63
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-10 11:51:12 +0300

    Added buttons row template for .md files for displaying buttons in one row

---
+++ src/components/buttonsRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="buttonsWrapper">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "buttonsRow"
+}
+</script>
+
+<style scoped>
+  .buttonsWrapper {
+    margin-top: 47px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    gap: 27px;
+  }
+</style>
