3e4960a0a3fe859051d327dafd3aabed8dffee31
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-10 11:51:52 +0300

    Added projects page

---
+++ src/pages/Projects.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+<!--    <template v:slot=title>{{$ts('Porjects')}}</template>-->
+    <div>simple div inside projects</div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: "Projects"
+}
+</script>
+
+<style scoped>
+
+</style>
