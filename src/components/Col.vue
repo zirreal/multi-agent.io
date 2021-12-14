47db0eecf02c5597270e6e17de413aad2cfcbad8
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-14 13:45:20 +0300

    Added rows and cols for tables

---
+++ src/components/Col.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-wrapper">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Col"
+}
+</script>
+
+<style scoped>
+  .col-wrapper {
+    width: 50%;
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+
+  }
+</style>
---
+++ src/components/Row.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="[{'centered': stickToTop !== undefined}, 'row-wrapper']">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Row",
+  props: ['stickToTop']
+}
+</script>
+
+<style scoped>
+  .row-wrapper {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    padding: 20px 0 20px 0;
+    gap: 15px;
+    align-items: flex-start;
+  }
+  .row-wrapper:not(:last-child) {
+    border-bottom: 1px solid black;
+  }
+  .centered {
+    align-items: center;
+  }
+</style>
