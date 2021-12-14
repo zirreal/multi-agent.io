d34705dc81eea27ed35096701dfd3745b2b91bad
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-14 13:39:53 +0300

    Added callout component for project page subtitle

---
+++ src/components/callout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="callout-wrapper">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "callout"
+}
+</script>
+
+<style scoped>
+  .callout-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    font-style: italic;
+    font-weight: 300;
+    font-size: 1.25rem;
+    margin-bottom: 50px;
+  }
+</style>
