5a6aba73115e7fe5ae75db3a73fb09bb2429f70f
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-14 13:43:12 +0300

    Added block for text after img block

---
+++ src/components/imgCaption.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="caption-wrapper">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "imgCaption"
+}
+</script>
+
+<style scoped>
+  .caption-wrapper {
+    margin-top: 50px;
+  }
+</style>
