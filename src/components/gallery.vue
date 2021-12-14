0c29d7966e9820e58d52512b25e9fdcc2597bb8b
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-14 13:43:39 +0300

    Added image gallery block

---
+++ src/components/gallery.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="gallery-wrapper">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gallery"
+}
+</script>
+
+<style scoped>
+  .gallery-wrapper {
+    margin-top: 50px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+  }
+  .gallery-wrapper > a {
+    display: inline-block;
+    background-size: cover;
+    margin-top: -13px;
+    position: relative;
+    width: 33.3333%;
+    height: 0;
+    padding-bottom: 33.3333%;
+  }
+  @media screen and (max-width: 500px){
+    .gallery-wrapper > a {
+      width: 50%;
+      padding-bottom: 50%;
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    .gallery-wrapper > a {
+      width: 100%;
+      padding-bottom: 100%;
+    }
+  }
+</style>
---
+++ src/components/galleryImg.vue
@@ -0,0 +1,21 @@
+<template>
+  <a class="gallery-image" target="_blank" :href="url" :style="style"></a>
+</template>
+
+<script>
+export default {
+  name: "galleryImg",
+  props: ['url'],
+  computed: {
+    style () {
+        return `background-image: url('${this.url}')`
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .gallery-image {
+    display: block;
+  }
+</style>
