99b3769e40a0da482abd2649894ec2b6df27570a
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-20 01:28:15 +0300

    Added default links and styles update for iconButton

--- src/components/iconButton.vue
+++ src/components/iconButton.vue
@@ -6,9 +6,10 @@
     <g-image v-if="type === 'Github' && imgHref === undefined" src="~/assets/images/GitHub.png"/>
     <g-image v-if="type === 'Youtube' && imgHref === undefined" src="~/assets/images/YouTube.png"/>
 
-
     <g-image v-if="imgHref !== undefined" :src="imgHref"/>
-    <slot/>
+    <div class="text-wrapper">
+      <slot/>
+    </div>
   </button>
 </template>
 
@@ -18,7 +19,28 @@ export default {
   props: ['imgHref', 'to', 'type'],
   methods: {
     redirect() {
-      window.location = this.$props.to
+      if(this.$props.to === '' || this.$props.to === undefined) {
+        console.log('switch is on')
+        switch (this.$props.type) {
+          case 'Github':
+            window.location = this.$static.metadata.link_github
+            break
+          case 'Youtube':
+            window.location = this.$static.metadata.link_youtube
+            break
+          case 'OpenSea':
+            window.location = this.$static.metadata.link_opensea
+            break
+          case 'Rarible':
+            window.location = this.$static.metadata.link_rarible
+            break
+          default:
+            window.location = '/'
+            break;
+        }
+      } else {
+        window.location = this.$props.to
+      }
     }
   }
 }
@@ -32,12 +54,34 @@ export default {
     font-size: 1rem;
     font-weight: bold;
     padding: 11px;
-    justify-content: center;
     align-items: center;
     gap: 11px;
+    transition: all .2s;
+    cursor: pointer;
   }
   button img {
     height: 29px;
     width: auto;
   }
+  .text-wrapper {
+    text-align: left;
+    flex-wrap: wrap;
+  }
+
+  button:hover {
+    border-color: var(--color-green);
+    color: var(--color-green);
+    /*filter: saturate(.4);*/
+  }
 </style>
+
+<static-query>
+  query {
+    metadata {
+      link_opensea,
+      link_youtube,
+      link_rarible,
+      link_github
+    }
+  }
+</static-query>
