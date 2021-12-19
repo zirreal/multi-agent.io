15543cb734661bb104dbe809487d6ccd088a81fb
IvanGlebov <glebov.vanya8@gmail.com>
2021-12-20 01:28:56 +0300

    Added global metadata with common links

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,9 +6,16 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+  api.loadSource(async store => {
+    store.addMetadata('link_github', 'https://github.com/Multi-Agent-io/gaka-chu.online')
+    store.addMetadata('link_rarible', 'https://rarible.com/gakachu')
+    store.addMetadata('link_opensea', 'https://opensea.io/accounts/Gaka-Chu')
+    store.addMetadata('link_youtube', 'https://www.youtube.com/channel/UCT5_U0Nddr9UG2Z15ArdOLA')
   })
+  // api.loadSource(({ addCollection }) => {
+
+    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+  // })
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
