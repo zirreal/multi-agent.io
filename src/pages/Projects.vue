eb2e45c2dbfdc9e236fda8ca34bbbc90c98cde6d
IvanGlebov <glebov.vanya8@gmail.com>
2022-01-07 02:10:10 +0300

    New Projects page

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,10 +1,16 @@
 <template>
   <Layout>
     <template v-slot:title>{{$ts('Projects')}}</template>
-    <h2>Our last projects</h2>
+    <p>Let us tell you about our R&D projects. All of them has scientific mission and real-life implementation. Some of them we can build for business, bring IoT to your life.</p>
     <div class="projectsGrid">
-      <div class="project-card" v-for="edge in $static.allPost.edges">
-        <a :href="'/projects/' + edge.node.title">{{edge.node.title}}</a>
+      <div class="project-card"
+           v-for="edge in $static.allPost.edges"
+           :style="{'background-image': 'url(' + edge.node.coverSrc + ')'}">
+        <p>
+          <a :href="'/projects/' + edge.node.title">{{edge.node.title}}</a>
+          <br/>
+          <a :href="'/projects/' + edge.node.title">{{edge.node.subtitle}}</a>
+        </p>
       </div>
     </div>
   </Layout>
@@ -18,21 +24,44 @@ export default {
 
 <style scoped>
   .projectsGrid {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-column-gap: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 65px;
   }
   .project-card {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #fafafa;
-    padding: 20px;
-    /*border-radius: 20px;*/
+    flex-direction: column-reverse;
     width: 100%;
-    height: 300px;
-    box-shadow: 1px 1px 20px #999999;
+    height: 442px;
+    /*background-image: url("/gakachuCover.png");*/
+    background-size: cover;
+    background-position: center;
   }
+
+  .project-card > p {
+    position: relative;
+    left: -30px;
+    bottom: 20px;
+    transition: all .3s;
+  }
+
+  .project-card > p > a {
+    background-color: var(--color-blue);
+    color: var(--color-light);
+    display: inline-block;
+    padding: 0 0.5rem;
+    font-size: 1.3rem;
+    font-weight: bold;
+    transition: all .3s;
+  }
+
+  .project-card > p:hover > a {
+    transition: all .3s;
+    background-color: var(--color-orange);
+    transform: translateX(10px);
+  }
+
 </style>
 
 <static-query>
@@ -41,6 +70,8 @@ export default {
       edges {
         node {
           title
+          subtitle
+          coverSrc
         }
       }
     }
