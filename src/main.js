340604ac4275ff491fee501c748551dba3ca6592
A. Bakai @positivecrash <nastin_box@list.ru>
2022-01-20 17:31:54 +0400

    YA tracker

--- src/main.js
+++ src/main.js
@@ -36,8 +36,16 @@ export default function (Vue, { router, head, isClient }) {
     src: 'https://www.googletagmanager.com/gtag/js?id=G-L4Z0XR6BR2',
     async: true
   })
-
   head.script.push({
     innerHTML: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-L4Z0XR6BR2');"
+  })
+
+  // Ya metrika
+  head.script.push({
+    type: "text/javascript",
+    innerHTML: '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(87222104, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});'
+  })
+  head.noscript.push({
+    innerHTML: '<div><img src="https://mc.yandex.ru/watch/87222104" style="position:absolute; left:-9999px;" alt="" /></div>'
   })
 }
