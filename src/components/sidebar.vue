c3ea5eb80ae3cb8e4d54e4ea6151e3b07eb1670c
A. Bakai @positivecrash <nastin_box@list.ru>
2022-01-20 14:50:38 +0400

    Fix routing

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -2,45 +2,43 @@
     <div class="sidebar">
 
         <p>
-            <g-link v-show="$locale=='en'" to="/" class="link-clear"> <logo /> </g-link>
-            <g-link v-show="$locale=='ru'" to="/ru/" class="link-clear"> <logo /> </g-link>
+            <g-link :to="localeSlash" class="link-clear"> <logo /> </g-link>
         </p>
 
         <p class="title">
-            <g-link to="/" exact v-show="$locale=='en'">
-                <span>Multi-Agent</span>
-                <span>Systems</span>
-            </g-link>
+            <g-link :to="localeSlash" exact>
+                <template v-if="$locale=='en'">
+                  <span>Multi-Agent</span>
+                  <span>Systems</span>
+                </template>
 
-            <g-link to="/ru" exact v-show="$locale=='ru'">
-                <span>Мультиагентные</span>
-                <span>системы</span>
+                <template v-if="$locale=='ru'">
+                  <span>Мультиагентные</span>
+                  <span>системы</span>
+                </template>
             </g-link>
         </p>
 
         <nav>
-            <p><g-link to="/about">{{$ts('About us')}}</g-link></p>
-            <p><g-link to="/projects">{{$ts('Projects')}}</g-link></p>
-            <!-- <div class="posts-col" v-if="$route.path.split('/')[1] === 'projects'">
-              <p v-for="edge in $static.sidebar.edges" :key="edge.node.id">
-                <g-link 
-                :to="edge.node.path"
-                v-if="edge.node.locale === $locale"
-                exact>
+            <div class="nav-line"><g-link :to="localeSlash + 'about'">{{$ts('About us')}}</g-link></div>
+            <div class="nav-line">
+              <g-link class="parent" :to="'/projects' + localeSlash">{{$ts('Projects')}}</g-link>
+              <div class="child">
+                <div v-for="edge in $static.sidebar.edges" :key="edge.node.id">
+                  <g-link 
+                  :to="edge.node.path"
+                  v-if="edge.node.locale === $locale"
+                  exact>
 
-                  {{edge.node.title}}
-                </g-link>
-              </p>
-            </div> -->
-            <p><g-link to="/jobs">{{$ts('Jobs')}}</g-link></p>
-            <p><g-link to="/contacts">{{$ts('Contacts')}}</g-link></p>
+                    {{edge.node.title}}
+                  </g-link>
+                </div>
+              </div>
+            </div>
+
+            <div class="nav-line"><g-link :to="localeSlash + 'jobs'">{{$ts('Jobs')}}</g-link></div>
+            <div class="nav-line"><g-link :to="localeSlash + 'contacts'">{{$ts('Contacts')}}</g-link></div>
         </nav>
-
-        <!-- <nav v-show="$locale=='ru'">
-            <p><g-link to="/ru/about">{{$ts('About us')}}</g-link></p>
-            <p><g-link to="/ru/contacts">{{$ts('Contacts')}}</g-link></p>
-            <p><g-link to="/ru/jobs">{{$ts('Jobs')}}</g-link></p>
-        </nav> -->
 
         <languageSwitcher />
     </div>
@@ -51,6 +49,15 @@ export default {
   components: {
     logo: () => import('../components/logo.vue'),
     languageSwitcher: () => import('../components/languageSwitcher.vue')
+  },
+  computed: {
+    localeSlash(){
+      if(this.$locale === 'en') {
+        return '/'
+      } else {
+        return '/' + this.$locale + '/'
+      }
+    }
   }
 }
 </script>
@@ -82,11 +89,21 @@ export default {
         background-color: var(--color-orange);
     }
 
-    nav .posts-col {
-      margin-bottom: var(--space)
+    .nav-line {
+      margin-bottom: var(--space);
     }
 
-    nav .posts-col a {
+    nav .child {
+      margin-top: var(--space);
+      margin-bottom: var(--space);
+      display: none;
+    }
+
+    nav .parent.active ~ .child {
+      display: block;
+    }
+
+    nav .child a {
       display: block;
       color: var(--color-dark);
       background-color: transparent;
@@ -94,13 +111,13 @@ export default {
       border-left: 4px solid var(--color-dark);
     }
     
-    nav .posts-col a.active {
+    nav .child a.active {
       color: var(--color-blue);
       border-color: var(--color-blue);
       cursor: default;
     }
 
-    nav .posts-col > p > a:hover:not(.active) {
+    nav .child > * > a:hover:not(.active) {
       color: var(--color-orange);
       border-color: var(--color-orange);
     }
